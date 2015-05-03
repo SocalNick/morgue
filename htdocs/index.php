2b1462b4a18e4c7b93482aa9fe90da5f7f7cab69
Nicholas Calugar <njcalugar@gmail.com>
2015-05-03 16:25:08 -0700

    Start locally with foreman
    - Change to application root
    - Include composer autoloading as soon as possible

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,10 +1,22 @@
 <?php
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
+
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+
+// Setup autoloading
+include 'vendor/autoload.php';
+
 require_once 'phplib/CurlClient.php';
 require_once 'phplib/Postmortem.php';
 require_once 'phplib/Configuration.php';
 require_once 'phplib/Auth.php';
-
-require_once 'vendor/autoload.php';
 
 @include 'phplib/deploy_version.php';
 
