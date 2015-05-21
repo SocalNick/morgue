146ec0f020ec584dbacc5057dcbf64e8657fb634
Nicholas Calugar <njcalugar@gmail.com>
2015-05-20 20:56:58 -0700

    WIP - Improvements
    - Correctly setting RelayState
    - Using samlNameId as Morgue username
    - Better logic in /saml/consume

--- phplib/SamlMiddleware.php
+++ phplib/SamlMiddleware.php
@@ -8,16 +8,16 @@ class SamlMiddleware extends Slim_Middleware {
 
         $pathInfo = $this->app->request()->getPathInfo();
 
-        if ($pathInfo != $this->app->urlFor('saml-acs') && !isset($_SESSION['samlUserdata'])) {
-          $settings = $this->app->samlSettings;
-          $authRequest = new OneLogin_Saml2_AuthnRequest($settings);
-          $samlRequest = $authRequest->getRequest();
-          $parameters = array('SAMLRequest' => $samlRequest);
-          $parameters['RelayState'] = OneLogin_Saml2_Utils::getSelfURLNoQuery();
-          $idpData = $settings->getIdPData();
-          $ssoUrl = $idpData['singleSignOnService']['url'];
-          $url = OneLogin_Saml2_Utils::redirect($ssoUrl, $parameters, true);
-          $this->app->redirect($url);
+        if ($pathInfo != $this->app->urlFor('saml-acs') && !isset($_SESSION['samlNameId'])) {
+            $settings = $this->app->samlSettings;
+            $authRequest = new OneLogin_Saml2_AuthnRequest($settings);
+            $samlRequest = $authRequest->getRequest();
+            $parameters = array('SAMLRequest' => $samlRequest);
+            $parameters['RelayState'] = $pathInfo;
+            $idpData = $settings->getIdPData();
+            $ssoUrl = $idpData['singleSignOnService']['url'];
+            $url = OneLogin_Saml2_Utils::redirect($ssoUrl, $parameters, true);
+            $this->app->redirect($url);
         }
 
         $this->next->call();
@@ -26,53 +26,30 @@ class SamlMiddleware extends Slim_Middleware {
 }
 
 function morgue_get_user_data() {
-    if (!isset($_SESSION['samlUserdata'])) {
+    if (!isset($_SESSION['samlNameId'])) {
         return;
     }
 
-    return;
-    // NEED TO GET USER NICKNAME
-    return array("username" => $userDetails->nickname);
+    return array("username" => $_SESSION['samlNameId']);
 }
 
 $app->post('/saml/consume', function() use ($app) {
-    try {
-        if (isset($_POST['SAMLResponse'])) {
-            $settings = $app->samlSettings;
-            $samlResponse = new OneLogin_Saml2_Response($settings, $_POST['SAMLResponse']);
-            if ($samlResponse->isValid()) {
-                echo 'You are: ' . $samlResponse->getNameId() . '<br>';
-                $attributes = $samlResponse->getAttributes();
-                $_SESSION['samlUserdata'] = $attributes;
-                $_SESSION['IdPSessionIndex'] = $samlResponse->getSessionIndex();
-                if (!empty($attributes)) {
-                    echo 'You have the following attributes:<br>';
-                    echo '<table><thead><th>Name</th><th>Values</th></thead><tbody>';
-                    foreach ($attributes as $attributeName => $attributeValues) {
-                        echo '<tr><td>' . htmlentities($attributeName) . '</td><td><ul>';
-                        foreach ($attributeValues as $attributeValue) {
-                            echo '<li>' . htmlentities($attributeValue) . '</li>';
-                        }
-                        echo '</ul></td></tr>';
-                    }
-                    echo '</tbody></table>';
-                }
+    if (!isset($_POST['SAMLResponse'])) {
+        throw new Exception('No SAML Response found in POST');
+    }
 
-                var_dump($_SESSION);
+    $settings = $app->samlSettings;
+    $samlResponse = new OneLogin_Saml2_Response($settings, $_POST['SAMLResponse']);
+    if (!$samlResponse->isValid()) {
+        throw new Exception('Invalid SAML Response');
+    }
 
-                if (isset($_POST['RelayState']) && OneLogin_Saml2_Utils::getSelfURL() != $_POST['RelayState']) {
-                    var_dump($_POST['RelayState']);
-                    die('redirect?');
-                    $auth->redirectTo($_POST['RelayState']);
-                }
-            } else {
-                echo 'Invalid SAML Response';
-            }
-        } else {
-            echo 'No SAML Response found in POST.';
-        }
-    } catch (Exception $e) {
-        echo 'Invalid SAML Response: ' . $e->getMessage();
+    $_SESSION['samlNameId'] = $samlResponse->getNameId();
+    $_SESSION['samlUserdata'] = $samlResponse->getAttributes();
+    $_SESSION['IdPSessionIndex'] = $samlResponse->getSessionIndex();
+
+    if (isset($_POST['RelayState']) && $_POST['RelayState'] != $app->request()->getPathInfo()) {
+        $app->redirect($_POST['RelayState']);
     }
 })->name('saml-acs');
 
